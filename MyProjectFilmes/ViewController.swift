8e49625b570ec2b7bc561d951bd2415af5521c19
asenasil <adalberto.sena.silva@everis.com>
2021-03-26 11:22:37 -0300

    Incluindo Projeto vazio

---
+++ MyProjectFilmes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyProjectFilmes
+//
+//  Created by Adalberto Sena Silva on 26/03/21.
+//  Copyright © 2021 filmes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
