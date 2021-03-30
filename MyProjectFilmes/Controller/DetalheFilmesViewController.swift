c67e548c270e1b6569394cb9663a827328b709ca
asenasil <adalberto.sena.silva@everis.com>
2021-03-30 18:44:05 -0300

    tentando colocar scrowview

--- MyProjectFilmes/Controller/DetalheFilmesViewController.swift
+++ MyProjectFilmes/Controller/DetalheFilmesViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class DetalheFilmesViewController: UIViewController, UINavigationControllerDelegate {
     
+    @IBOutlet weak var scrollview: UIStackView!
+    
     @IBOutlet weak var tituloDetalhes: UILabel!
     
     @IBOutlet weak var imagemDetalhes: UIImageView!
@@ -36,6 +38,9 @@ class DetalheFilmesViewController: UIViewController, UINavigationControllerDeleg
         tituloDetalhes.text = informacaoFilmeSelecionado.originalTitle
         sinopseDetalhes.text = informacaoFilmeSelecionado.overview
         lancamentoDetalhes.text = "\(informacaoFilmeSelecionado.voteAverage)"
+        
+        //self.scrollview.contentSize = CGSize(width: self.scrollview.frame.width, height: self.scrollview.frame.height + 320)
+        
         
         let urlCartaz = URL(string: "https://image.tmdb.org/t/p/w300/\(informacaoFilmeSelecionado.backdropPath)")
         
